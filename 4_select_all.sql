ea19fd1ab7d8c896606b6e568668d8463baeb771
AnceTetere <ance.tetere@outlook.com>
2023-10-26 17:07:04 +0300

    PostgreSQL SELECT queries.

---
+++ 4_select_all.sql
@@ -0,0 +1,5 @@
+SELECT date_received, product_name, sub_product, issue, sub_issue, consumer_complaint_narrative, company_public_response, company, state_name, zip_code, tags, consumer_consent_provided, submitted_via, date_sent, company_response_to_consumer, timely_response, consumer_disputed, complaint_id
+FROM consumer_complaints;
+
+SELECT *
+FROM consumer_complaints;
---
+++ 6_select_3columns.sql
@@ -0,0 +1,2 @@
+SELECT company, zip_code, state_name
+FROM consumer_complaints;
