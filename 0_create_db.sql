faae4377f915188421ed4bea6ef48061313c8be7
AnceTetere <ance.tetere@outlook.com>
2023-10-26 16:55:01 +0300

    Created database

---
+++ 0_create_db.sql
@@ -0,0 +1,13 @@
+-- Database: consumer complaints
+
+-- DROP DATABASE IF EXISTS "consumer complaints";
+
+CREATE DATABASE "consumer complaints"
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_United Kingdom.1252'
+    LC_CTYPE = 'English_United Kingdom.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
